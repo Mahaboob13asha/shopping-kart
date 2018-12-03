1b25c23f0cd59781a119f60f0f78feda4cb86353
mahaboob <mahaboob.basha@atsglobal.in>
2018-12-03 14:46:55 +0530

    New projects

---
+++ shoppingkart/src/main/java/com/ats/shoppingkart/shoppingkart/App.java
@@ -0,0 +1,13 @@
+package com.ats.shoppingkart.shoppingkart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ shoppingkartbkend/src/main/java/com/ats/shoppingkartbkend/shoppingkartbkend/App.java
@@ -0,0 +1,13 @@
+package com.ats.shoppingkartbkend.shoppingkartbkend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
